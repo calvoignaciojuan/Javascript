47816fe2aaa732a3afce48624e1bbe29d613f68c
calvoignaciojuan <calvoignaciojuan@gmail.com>
2023-08-05 19:10:59 -0300

    foreach

---
+++ Arrays.js
@@ -0,0 +1,15 @@
+
+
+
+const arreglo = [0,1,2,3,4,5,6,7,8,9];
+
+// FOREACH
+
+//return undefined (no retorna nada, sirve para hacer algo con cada elemento)
+arreglo.forEach((element,index,arr) =>{
+
+    console.log('element',element);
+    console.log('index',index);
+    console.log('arr',arr);
+});
+
